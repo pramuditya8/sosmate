854888f5b67a5e9e1e91d7b0ff76eddad2546396
pramuditya8 <pramuditya8@github.com>
2023-03-09 12:20:30 +0700

    login register

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,14 @@ router.post("/register", UserController.userRegisterPost)
 router.get("/login", UserController.userLoginForm)
 router.post("/login", UserController.userLoginPost)
 
+router.use(function (req, res, next) {
+  console.log(req.session);
+  
+    next();
+  
+});
+
+router.get("/home", (req, res) => res.send("hello world"))
+
+
 module.exports = router
