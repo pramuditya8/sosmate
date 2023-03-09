e598e762ad487654e50ce38489087dda21f990ae
Muhammad zidhan <119053048+muhamadzidhan@users.noreply.github.com>
2023-03-09 14:39:00 +0700

    coommit

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -3,57 +3,71 @@ const bcrypt = require("bcryptjs")
 
 class UserController {
 
-  static userLoginForm(req, res){
-    res.render("auth/login")
+  static userLoginForm(req, res) {
+    const { error } = req.query
+    res.render("auth/login", { error })
   }
 
-  static userLoginPost(req, res){
-    const {email, password} = req.body
-    User.findOne({where: {email}})
-    .then(user => {
-      const error = "Email or Password Invalid"
+  static userLoginPost(req, res) {
+    const { email, password } = req.body
+    User.findOne({ where: { email } })
+      .then(user => {
+        const error = "Email or Password Invalid"
 
-      if(user){
-        const isValidPassword = bcrypt.compareSync(password, user.password); // true or false
+        if (user) {
+          const isValidPassword = bcrypt.compareSync(password, user.password); // true or false
 
-        if(isValidPassword){
-          req.session.UserId = user.id;
-          req.session.username = user.username;
-          req.session.role = user.role; // set session di controller
-          req.session.isLogin = true
+          if (isValidPassword) {
+            req.session.UserId = user.id;
+            req.session.username = user.username;
+            req.session.role = user.role; // set session di controller
+            req.session.isLogin = true
 
-          return res.redirect(`/home`)
-        }else{
+            return res.redirect(`/home`)
+          } else {
+            return res.redirect(`/login?error=${error}`)
+          }
+
+
+        } else {
+
           return res.redirect(`/login?error=${error}`)
         }
-
-
-      }else{
-
-        return res.redirect(`/login?error=${error}`)
-      }
-    })
-    .catch(err =>{
-      console.log("test")
-      res.send(err)
-    })
+      })
+      .catch(err => {
+        if (err.name == "SequelizeValidationError") {
+          const message = err.errors.map(el => {
+            return el.message
+          })
+          res.send(message)
+        }
+      })
   }
 
-  static userRegisterForm(req, res){
-    res.render("auth/register")
+  static userRegisterForm(req, res) {
+    const { errors } = req.query
+    res.render("auth/register", { errors })
   }
 
-  static userRegisterPost(req, res){
+  static userRegisterPost(req, res) {
     console.log(req.body)
     const { username, email, password, role } = req.body
 
-    User.create({username, email, password, role})
-    .then(data => {
-      res.redirect("/login")
-    })
+    User.create({ username, email, password, role })
+      .then(data => {
+        res.redirect("/login")
+      })
+      .catch(err => {
+        if (err.name == "SequelizeValidationError") {
+          const message = err.errors.map(el => el.message)
+          res.redirect(`/register?errors=${message}`)
+        } else {
+          res.send(err)
+        }
+      })
   }
 
-  
+
 }
 
 module.exports = UserController
